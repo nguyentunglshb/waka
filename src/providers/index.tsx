5eb901a77a23f4fc054ace7ca51c5c0d80f16849
Tung Nguyen <tungnv@vega.com.vn>
2024-07-17 08:30:21 +0700

    add login form

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,13 +1,17 @@
 "use client";
 
 import { SessionProvider } from "next-auth/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import QueryProvider from "./query-provider";
 
 const Provider = ({ children }: { children: React.ReactNode }) => {
   return (
-    <SessionProvider>
-      <QueryProvider>{children}</QueryProvider>
-    </SessionProvider>
+    <>
+      <SpeedInsights />
+      <SessionProvider>
+        <QueryProvider>{children}</QueryProvider>
+      </SessionProvider>
+    </>
   );
 };
 
